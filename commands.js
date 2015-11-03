a56820a51259b1d156a0c946c9ba110bbe3fc078
Parnassius <luca.fausto@gmail.com>
2015-11-03 22:38:30 +0100

    update usage stats link

--- commands.js
+++ commands.js
@@ -1666,7 +1666,7 @@ exports.commands = {
 	
 	stats: function(arg, by, room, con) {
 		if (this.canUse('broadcast', room, by) || room.charAt(0) === ',') {
-			return this.say(con, room, "http://www.smogon.com/stats/2015-09/");
+			return this.say(con, room, "http://www.smogon.com/stats/2015-10/");
 		}
 	},
 	
