2c07308326fafb569e05ead373b42862932be7a8
Parnassius <lucasiregistraaisiti@protonmail.ch>
2016-04-09 11:30:37 +0200

    update usage stats link

--- commands.js
+++ commands.js
@@ -1450,7 +1450,7 @@ exports.commands = {
 	
 	stats: function(arg, by, room, con) {
 		if (this.canUse('broadcast', room, by) || room.charAt(0) === ',') {
-			return this.say(con, room, "http://www.smogon.com/stats/2016-02/");
+			return this.say(con, room, "http://www.smogon.com/stats/2016-03/");
 		}
 	},
 	
